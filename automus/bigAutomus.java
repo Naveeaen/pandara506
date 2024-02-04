6fc0cd9f06b7967aaab902969b0b6ea66e7a3f6b
MCSpoooof <cachemaola@gmail.com>
2024-02-04 15:46:39 -0500

    b,kb

--- automus/bigAutomus.java
+++ automus/bigAutomus.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "mainAuto")
-public class mainAuto extends LinearOpMode {
+public class bigAutomus extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
     }
