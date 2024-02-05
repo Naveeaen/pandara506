e8e2b2d8c5ae49b4a0d36531d0554b2854ba7671
Naveen <57783716+Naveeaen@users.noreply.github.com>
2024-02-04 19:22:27 -0500

    big automus almost done 02/04 but actually

--- automus/bigAutomus.java
+++ automus/bigAutomus.java
@@ -8,41 +8,50 @@ import org.firstinspires.ftc.teamcode.pandara506.roadrunner.Hardware;
 
 @Autonomous(name = "bigAuto")
 public class bigAutomus extends LinearOpMode {
+    Hardware drive = new Hardware(hardwareMap);
+    automusBlueFront autoBF = new automusBlueFront();
+    automusBlueBack autoBB = new automusBlueBack();
+    automusRedFront autoRF = new automusRedFront();
+    automusRedFrontGolden autoRFGF = new automusRedFrontGolden();
+    automusRedBackJava autoRB = new automusRedBackJava();
+
+    String color = "";
+    String side = "";
+    int timeout = 0;
+    String cycle = "";
+    String park = "";
+
+    boolean Cswitch = false;
+    boolean Sswitch = false;
+    boolean Cycswitch = false;
+    boolean Pswitch = false;
+
+    boolean Rbumped = false;
+    boolean Lbumped = false;
+    boolean Rtriggered = false;
+    boolean Ltriggered = false;
+    boolean Bpressed = false;
+    boolean Xpressed = false;
+
     public void runOpMode() throws InterruptedException {
-        Hardware drive = new Hardware(hardwareMap);
-        automusBlueFront autoBF = new automusBlueFront();
-        automusBlueBack autoBB = new automusBlueBack();
-        automusRedFront autoRF = new automusRedFront();
-        automusRedFrontGolden autoRFGF = new automusRedFrontGolden();
-        automusRedBackJava autoRB = new automusRedBackJava();
-
-        String color = "";
-        String side = "";
-        int timeout = 0;
-
-        boolean Cswitch = false;
-        boolean Sswitch = false;
-        boolean Tswitch = false;
-
-        boolean Rbumped = false;
-        boolean Lbumped = false;
-        boolean Rtriggered = false;
 
         while(opModeInInit()) {
 
             telemetry.addData("Color", color);
             telemetry.addData("Side", side);
             telemetry.addData("Timeout", timeout);
+            telemetry.addData("Cycling", cycle);
+            telemetry.addData("Park", park);
             telemetry.update();
 
             //color switch (lol)
             if (gamepad1.right_bumper && !Rbumped) {
-                if (Cswitch) {
-                    color = "Red";
-                    Cswitch = false;
+                if (Sswitch) {
+                    side = "Front";
+                    Sswitch = false;
                 } else {
-                    color = "Blue";
-                    Cswitch = true;
+                    side = "Back";
+                    Sswitch = true;
                 }
                 Rbumped = true;
             } else if (!gamepad1.right_bumper) {
@@ -51,38 +60,79 @@ public class bigAutomus extends LinearOpMode {
 
             // side switch
             if (gamepad1.left_bumper && !Lbumped) {
-                if (Sswitch) {
-                    side = "Front";
-                    Sswitch = false;
+                if (Cswitch) {
+                    color = "Red";
+                    Cswitch = false;
                 } else {
-                    side = "Back";
-                    Sswitch = true;
+                    color = "Blue";
+                    Cswitch = true;
                 }
                 Lbumped = true;
             } else if (!gamepad1.left_bumper) {
                 Lbumped = false;
             }
 
-            // timeout select
+            // timeout +
             if (gamepad1.right_trigger != 0 && !Rtriggered) {
-                if (Tswitch) {
-                    timeout += 500 * gamepad1.right_trigger;
-                    Tswitch = false;
-                } else {
-                    timeout -= 500 * gamepad1.right_trigger;
-                    Tswitch = true;
-                }
+                timeout += 100 * gamepad1.right_trigger;
                 Rtriggered = true;
             } else if (gamepad1.right_trigger == 0) {
                 Rtriggered = false;
             }
+            //timeout -
+            if (gamepad1.left_trigger != 0 && !Ltriggered) {
+                timeout -= 100 * gamepad1.left_trigger;
+                Ltriggered = true;
+            } else if (gamepad1.left_trigger == 0) {
+                Ltriggered = false;
+            }
 
-            waitForStart();
+            // cycle switch
+            if (gamepad1.b && !Bpressed) {
+                if (Cycswitch) {
+                    cycle = "yes";
+                    Cycswitch = false;
+                } else {
+                    cycle = "no";
+                    Cycswitch = true;
+                }
+                Bpressed = true;
+            } else if (!gamepad1.b) {
+                Bpressed = false;
+            }
+
+            // side switch
+            if (gamepad1.x && !Xpressed) {
+                if (Pswitch) {
+                    park = "Red";
+                    Pswitch = false;
+                } else {
+                    park = "Blue";
+                    Pswitch = true;
+                }
+                Xpressed = true;
+            } else if (!gamepad1.x) {
+                Xpressed = false;
+            }
+
             autoBF.timeout = timeout;
             autoBB.timeout = timeout;
             autoRF.timeout = timeout;
             autoRB.timeout = timeout;
             autoRFGF.timeout = timeout;
+
+            autoBF.cycle = cycle;
+            autoBB.cycle = cycle;
+            autoRF.cycle = cycle;
+            autoRB.cycle = cycle;
+            autoRFGF.cycle = cycle;
+
+            autoBF.park = park;
+            autoBB.park = park;
+            autoRF.park = park;
+            autoRB.park = park;
+            autoRFGF.park = park;
+
 
             if(color.equals("Red") && side.equals("Front")) autoRF.runOpMode();
             if(color.equals("Red") && side.equals("Back")) autoRF.runOpMode();
